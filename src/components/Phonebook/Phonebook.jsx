0dd39b9c5c9d85f77746a60f6ff1b8371e4b8a61
exigibil <adrian.topa04@gmail.com>
2024-04-18 11:18:56 +0300

    update contact list

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -4,20 +4,48 @@ import { nanoid } from 'nanoid';
 
 function Phonebook() {
   return (
-    <div className={styles.phonebookContainer}>
+    <>
+      <div className={styles.phonebookContainer}>
         <p className={styles.title}>Name</p>
-      <input
-        className={styles.inputText}
-        type="text"
-        name="name"
-        pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
+        <input
+          className={styles.inputText}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder="Enter your name"
+        />
 
-      <button className={styles.buttonContact} type="submit">Add contact</button>
-    </div>
+        <p className={styles.title}>Phone Number</p>
+        <input
+          className={styles.inputText}
+          type="telephone"
+          name="phone number"
+          pattern="number"
+          title="Phone number must be a number"
+          placeholder="Enter your phone number"
+          required
+        />
+        <button className={styles.buttonContact} type="submit">
+          Add contact
+        </button>
+      </div>
+      <div className={styles.phonebookContainer}>
+        <h2 className={styles.title}>Contacts List</h2>
+
+        <input 
+        className={styles.inputText}
+        type="text" 
+        placeholder="Search.." />
+
+        <div>
+          Lista
+        </div>
+
+      </div>
+    </>
   );
 }
 
-export default Phonebook
+export default Phonebook;
